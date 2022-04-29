e4969a9f2f06736cfcdb5aa4948ee6f39ebdf4c0
jarenreber <jaren.c.reber@gmail.com>
2022-04-29 16:04:23 -0600

    added tests to functions.test.js

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,14 @@
 const {shuffleArray} = require('./utils')
 
+let testArr = [1,2,3,4]
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('shuffled array returns an array', () => {
+        expect(Array.isArray(shuffleArray(testArr))).toBe(true)
+    })
+
+    test('should check that all the same items are in the array', () => {
+        expect(shuffleArray(testArr)).toEqual(expect.arrayContaining(testArr))
+    })
 })
+
